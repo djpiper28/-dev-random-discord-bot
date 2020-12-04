cff51093cb1e9c656e69fb750e1609b559318440
Danny Piper <ddjpiper28@gmail.com>
2020-12-04 10:51:13 +0000

    Bot now turns on

--- app/src/main/java/dev/random/discord/bot/App.java
+++ app/src/main/java/dev/random/discord/bot/App.java
@@ -3,8 +3,27 @@
  */
 package dev.random.discord.bot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
         //Start
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+
+        // Disable parts of the cache
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        // Enable the bulk delete event
+        builder.setBulkDeleteSplittingEnabled(false);
+        // Disable compression (not recommended)
+        builder.setCompression(Compression.NONE);
+        // Set activity (like "playing Something")
+        builder.setActivity(Activity.watching("TV"));
+
+        builder.build();
     }
 }
