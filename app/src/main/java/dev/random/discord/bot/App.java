7fe44306e7cd526f8ca5d02fe14ed83d00c7ad4a
Danny Piper <ddjpiper28@gmail.com>
2020-12-04 11:17:51 +0000

    Added message listener

--- app/src/main/java/dev/random/discord/bot/App.java
+++ app/src/main/java/dev/random/discord/bot/App.java
@@ -6,13 +6,17 @@ package dev.random.discord.bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
 public class App {
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws LoginException, InterruptedException {
         //Start
         JDABuilder builder = JDABuilder.createDefault(args[0]);
 
@@ -27,5 +31,16 @@ public class App {
 
         JDA jda = builder.build();
         jda.setAutoReconnect(true);
+        jda.awaitReady();
+
+        jda.addEventListener(new ListenerAdapter() {
+            @Override
+            public void onMessageReceived(MessageReceivedEvent event) {
+                if (!event.getAuthor().isBot()) {
+                    String msg = event.getMessage().getContentRaw();
+                }
+            }
+        });
     }
+
 }
